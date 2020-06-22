9a9704df255df2ae742e5ad1740f12bcba377bf4
Anand-Ochir <anand-ochir@Anand-Ochir.local>
2020-06-22 17:21:36 +0800

    test

--- src/components/todo/add-todo.jsx
+++ src/components/todo/add-todo.jsx
@@ -1,11 +1,17 @@
 import React, { useContext } from 'react';
 
 const AddTodo = () => {
-    let tdata = useContext(todo)
-    let pdata = useContext(prog)
-    let ddata = useContext(done)
+    let tdata = useContext(todo);
+    let pdata = useContext(prog);
+    let ddata = useContext(done);
 
-    
+    tdata.forEach(el => {
+        return (
+            <div className='todo'>
+                {el}
+            </div>
+        )
+    })
     
     // console.log(data)
     // return (
